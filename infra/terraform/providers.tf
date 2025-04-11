ffdce7e6f532d73108ae915467f773297857fd6b
Carlos <cmardonespino@gmail.com>
2025-04-11 12:27:18 -0400

    terraform fmt

--- infra/terraform/providers.tf
+++ infra/terraform/providers.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-  region = "us-east-1"
+  region  = "us-east-1"
   profile = "devops"
 }
 
 terraform {
   backend "s3" {
-    bucket = "cmardonesp-terraform-state"
-    key    = "devexhiringtest/cmardonespino/aws"
-    region = "us-east-1"
+    bucket  = "cmardonesp-terraform-state"
+    key     = "devexhiringtest/cmardonespino/aws"
+    region  = "us-east-1"
     profile = "devops"
   }
 }
