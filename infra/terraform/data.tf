3d8d24cddb2e9ce2fb02e7527a2d22ca6aaeaf7b
Carlos <cmardonespino@gmail.com>
2025-04-11 12:33:23 -0400

    terraform fmt

--- infra/terraform/data.tf
+++ infra/terraform/data.tf
@@ -1,6 +1,6 @@
 data "aws_subnets" "private" {
-    filter {
-        name = "tag:Name"
-        values = ["ws-test-subnet-private*-us-east-1*"]
-    }
+  filter {
+    name   = "tag:Name"
+    values = ["ws-test-subnet-private*-us-east-1*"]
+  }
 }
--- infra/terraform/eks.tf
+++ infra/terraform/eks.tf
@@ -1,5 +1,5 @@
 module "eks_devexhiringtest_cluster" {
-    source = "./modules/aws_eks"
-    eks_cluster_name = "devexhiringtest-cmardonesp"
-    eks_cluster_subnet_ids = data.aws_subnets.private.ids
+  source                 = "./modules/aws_eks"
+  eks_cluster_name       = "devexhiringtest-cmardonesp"
+  eks_cluster_subnet_ids = data.aws_subnets.private.ids
 }
